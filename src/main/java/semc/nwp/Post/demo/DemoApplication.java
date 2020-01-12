6ac07d44ab3a613a9fc3677cad89ec230fe8555d
Marko Grozdanović <grozd27@gmail.com>
2020-01-12 15:17:51 +0100

    Init

---
+++ src/main/java/semc/nwp/Post/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package semc.nwp.Post.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/semc/nwp/Post/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package semc.nwp.Post.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+
+}
