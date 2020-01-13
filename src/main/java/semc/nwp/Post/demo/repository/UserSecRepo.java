0fbfc563a601c3c6138d0c4328185ba314737137
Marko Grozdanović <grozd27@gmail.com>
2020-01-13 13:33:36 +0100

    uklonjen je security

--- src/main/java/semc/nwp/Post/demo/repository/UserSecRepo.java
+++ src/main/java/semc/nwp/Post/demo/repository/UserSecRepo.java
@@ -3,8 +3,9 @@ package semc.nwp.Post.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import semc.nwp.Post.demo.model.User;
 
-public interface UserSecRepo extends JpaRepository<Integer, User> {
+public interface UserSecRepo extends JpaRepository<User, Integer> {
 
 
-    User findByusernameAndpassword(String username, String password);
+
+    User findByUsername(String username);
 }
